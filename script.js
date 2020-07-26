90b500877b88dae7f1bb0e65c569cdb9af9f0b14
victor-mirgichan <provic13@gmail.comcd>
2020-07-26 21:03:49 +0300

    styiling portfolio

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
     $("#development-image").click(function(){
       $("#development-image").slideDown('1500').hide('1000');
@@ -9,3 +8,46 @@ $(document).ready(function(){
       $("#development-image").slideDown('1500');
     });
   });
+  
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
+
+  $(document).ready(function(){
+    $("#work1").mouseover(function(){
+      $("#overlay").show();
+    }).mouseout(function(){
+      $("#overlay").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work2").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#work3").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
