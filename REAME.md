34e571b05afd04958a2263b728262d9b44b3a643
victor-mirgichan <provic13@gmail.comcd>
2020-07-27 13:58:29 +0300

    added read me and licence

---
+++ REAME.md
@@ -0,0 +1,22 @@
+# DELANI STUDIO
+
+#### Author: Victor Fereiti Mirgichan 
+
+### Description: This is a portfolio website of delani studio.
+### A Link to published site [https://victor-mirgichan.github.io/Akan-name/]
+
+
+### project setup instruction: 
+
+
+
+## Technologies Used
+* HTML
+* CSS
+* Bootsrap
+* JQuery
+* Javascript
+
+## Support and contact details
+contact me provic13@gmail.com
+#### licence and copyright informatioN, LICENCED UNDER MIT ()
--- script.js
+++ script.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
       $("#development-image").slideDown('1500');
     });
   });
-  
   $(document).ready(function(){
     $("#design-image").click(function(){
       $("#design-image").slideDown('1500').hide('1000');
@@ -18,8 +17,7 @@ $(document).ready(function(){
       $("#design").slideUp('1500');
       $("#design-image").slideDown('1500');
     });
-  });
-  
+  });  
   $(document).ready(function(){
     $("#product-image").click(function(){
       $("#product-image").slideDown('1500').hide('1000');
@@ -30,24 +28,53 @@ $(document).ready(function(){
       $("#product-image").slideDown('1500');
     });
   });
-
-  $(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
-    });
+ $(document).ready(function(){
+  $("#work1").mouseover(function(){
+    $("#overlay").show();
+  }).mouseout(function(){
+    $("#overlay").hide();
   });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
-    });
+});
+$(document).ready(function(){
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
   });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
+});
+$(document).ready(function(){
+  $("#work3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work5").mouseover(function(){
+    $("#overlay5").show();
+  }).mouseout(function(){
+    $("#overlay5").hide();
+  });
+  $("#work6").mouseover(function(){
+    $("#overlay6").show();
+  }).mouseout(function(){
+    $("#overlay6").hide();
+  });
+  $("#work7").mouseover(function(){
+    $("#overlay7").show();
+  }).mouseout(function(){
+    $("#overlay7").hide();
+  });
+  $("#work8").mouseover(function(){
+    $("#overlay8").show();
+  }).mouseout(function(){
+    $("#overlay8").hide();
+  });
+});
